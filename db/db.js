e175be7d09340bedd0ee001b3e9a6c4ba60c9e1a
RALLAPALLI V S B HARSHITH <harshithrvsb@gmail.com>
2025-01-27 23:04:00 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,7 +1,13 @@
 import mongoose from 'mongoose'
 export const connectDB=async()=>{
     try {
-        const conn=await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        const conn=await mongoose.connect(process.env.MONGO_URI, 
+                                          {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // 30 seconds timeout
+      connectTimeoutMS: 30000,         // Connection timeout
+    });
 ;
         console.log(`MongoDB connected: ${conn.connection.host}`)
     } catch (error) {
