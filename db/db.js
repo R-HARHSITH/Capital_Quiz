3764373ae458341966c062af6022b34a4484bbf6
RALLAPALLI V S B HARSHITH <harshithrvsb@gmail.com>
2025-01-27 22:44:59 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 export const connectDB=async()=>{
     try {
-        const conn=await mongoose.connect(process.env.MONGO_URI);
+        const conn=await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+;
         console.log(`MongoDB connected: ${conn.connection.host}`)
     } catch (error) {
         console.log("MongoDB connection error:",error);
